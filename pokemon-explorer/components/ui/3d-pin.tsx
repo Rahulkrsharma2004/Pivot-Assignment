15ca17aea65974566a55e238a9ee71c506284653
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-01-20 21:09:32 +0530

    some changes

--- pokemon-explorer/components/ui/3d-pin.tsx
+++ pokemon-explorer/components/ui/3d-pin.tsx
@@ -31,7 +31,7 @@ export const PinContainer = ({
   return (
     <Link
       className={cn(
-        "relative group/pin z-50 cursor-pointer overflow-hidden", 
+        "relative  z-50 cursor-pointer overflow-hidden", 
         containerClassName
       )}
       onMouseEnter={onMouseEnter}
